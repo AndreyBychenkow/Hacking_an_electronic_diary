7943d501f059fa8828e4961e3c57214272bd7107
AndreyBychenkow <decebell032@yandex.ru>
2024-10-13 22:12:35 +0300

    Created an auxiliary function, reduced the number of queries to the database

--- scripts.py
+++ scripts.py
@@ -1,22 +1,28 @@
 from datacenter.models import Schoolkid, Chastisement, Lesson, Commendation
 
 # Настройки изменений
-FULL_NAME = 'Рябова София Ильинична'
+FULL_NAME = 'Белозеров Авдей Федотович'
 SUBJECT_TITLE = 'История'
-PRAISE_TEXT = 'Отлично справилась,скрипт работает)))!'
+PRAISE_TEXT = 'Отлично справилась,гусь, скрипт работает)))!'
 BAD_GRADES = [2, 3]
 NEW_GRADE = 5
 
 
-# Функция удаления замечаний ученика по полному имени(ФИО).
-def remove_chastisements(full_name):
+# Вспомогательная функция для точного нахождения ученика по ФИО
+def get_schoolkid(full_name):
     schoolkid = Schoolkid.objects.filter(full_name=full_name).first()
     if not schoolkid:
         print(f'Ученик с полным именем "{full_name}" не найден.')
+    return schoolkid
+
+
+# Функция удаления замечаний ученика по полному имени(ФИО).
+def remove_chastisements(full_name):
+    schoolkid = get_schoolkid(full_name)
+    if not schoolkid:
         return
-    comments = Chastisement.objects.filter(schoolkid=schoolkid)
-    if comments.exists():
-        comments.delete()
+    deleted_count, _ = Chastisement.objects.filter(schoolkid=schoolkid).delete()
+    if deleted_count > 0:
         print(f'Все замечания для {schoolkid.full_name} были удалены.')
     else:
         print(f'У {schoolkid.full_name} нет замечаний.')
@@ -27,15 +33,11 @@ remove_chastisements(FULL_NAME)
 
 # Функция для исправления плохих оценок ученика по полному имени(ФИО).
 def fix_marks(full_name):
-    schoolkid = Schoolkid.objects.filter(full_name=full_name).first()
+    schoolkid = get_schoolkid(full_name)
     if not schoolkid:
-        print(f'Ученик с полным именем "{full_name}" не найден.')
         return
-    bad_marks = schoolkid.mark_set.filter(points__in=BAD_GRADES)
-    if bad_marks.exists():
-        for mark in bad_marks:
-            mark.points = NEW_GRADE
-            mark.save()
+    updated_count = schoolkid.mark_set.filter(points__in=BAD_GRADES).update(points=NEW_GRADE)
+    if updated_count > 0:
         print(f'Все плохие оценки для {schoolkid.full_name} были исправлены.')
     else:
         print(f'У {schoolkid.full_name} нет плохих оценок.')
@@ -46,12 +48,11 @@ fix_marks(FULL_NAME)
 
 # Функция добавления хорошего отзыва и предмета ученика по полному имени(ФИО).
 def create_commendation(full_name, subject_title):
-    schoolkid = Schoolkid.objects.filter(full_name=full_name).first()
+    schoolkid = get_schoolkid(full_name)
     if not schoolkid:
-        print(f'Ученик с полным именем "{full_name}" не найден.')
         return
-    lesson = Lesson.objects.filter(subject__title=subject_title, group_letter='А', year_of_study=6).order_by(
-        '-date').first()
+    lesson = Lesson.objects.filter(subject__title=subject_title, group_letter='А', year_of_study=6).select_related(
+        'teacher').order_by('-date').first()
     if lesson:
         Commendation.objects.create(
             text=PRAISE_TEXT,
